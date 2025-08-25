f5174dd9e85526aafc7d7c8046487907b80872f4
Douglas Minoru Kondo <douglas.minoru@gmail.com>
2025-08-24 23:51:03 -0300

    Add suport agents to extract and load data.

--- Aula17-Desafio4/extractor-load-data.py
+++ Aula17-Desafio4/extractor-load-data.py
@@ -9,6 +9,8 @@ import unicodedata
 import datetime
 from dotenv import load_dotenv
 from dataclasses import dataclass, field
+
+from pandas.core.interchange.dataframe_protocol import DataFrame
 from pydantic import BaseModel
 from pydantic_ai import Agent, RunContext, format_as_xml
 from typing import List, Dict
@@ -96,9 +98,147 @@ def create_table(db_conn, arquivo_xlsx, nome_tabela):
 class SQLQuery:
     query:str
 
+@dataclass
+class XLSFileList:
+    xlslist: List[str] = field(default_factory=list)
+
+class XLSFileListOutput(BaseModel):
+    output_message: str
+    filelist: List[str]
+
+class TableListOutput(BaseModel):
+    output_message: str
+    tablelist: List[str]
+
+def agente_extracao(zip_file: str) -> XLSFileListOutput:
+    model = GoogleModel(
+        'gemini-2.0-flash', provider=GoogleProvider(api_key=os.environ.get('gem_key'))
+    )
+
+    agente = Agent(
+        model,
+        deps_type=str,
+        retries=1,
+        output_type=XLSFileListOutput,
+        system_prompt=("Você é um analista de suporte que receberá o endereço de um arquivo do tipo .ZIP "
+                       "e utilizará a função 'tool_extrair_zip' informando como parâmetro o nome do arquivo a ser descompactado "
+                       "e retornará como resultado a lista de arquivos descompactados."
+       )
+    )
+
+    @agente.tool
+    def tool_extrair_zip(ctx: RunContext[str], filename:str) -> List[str]:
+        try:
+            print(f'   tool_extrair_zip: {filename}')
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                zip_ref.extractall(DIR_EXTRACAO)
+
+            # Encontra o primeiro arquivo CSV extraído
+            filelist = []
+            for arquivo in os.listdir(DIR_EXTRACAO):
+                if arquivo.endswith('.xlsx'):
+                    filelist.append(f'{DIR_EXTRACAO}/{arquivo}')
+            return filelist
+        except Exception as ex:
+            print(f'@@@ Error ao extrair arquivo ZIP.\n'
+                  f'   @@@ {type(ex)}\n'
+                  f'   @@@{ex=}')
+
+    retorno_ext = agente.run_sync(zip_file, deps=zip_file)
+    return retorno_ext.output
+
+def agente_load(xlslist: List[str]) -> List[str]:
+    model = GoogleModel(
+        'gemini-2.0-flash', provider=GoogleProvider(api_key=os.environ.get('gem_key'))
+    )
+
+    agente = Agent(
+        model,
+        retries=1,
+        deps_type=XLSFileList,
+        output_type=TableListOutput,
+        system_prompt=(
+            """### PAPEL: Você é um analista de suporte especializado em receber uma lista de planilhas, avaliar seu conteúdo e criar as tabelas em SQLite com a devida carga dos dados recebidos nas mesmas. 
+               ### OPBJETIVO: Utilizando apenas a lista de arquivos recebida, você deverá iterar por cada planilha XLS, pegar seu conteúdo através da tool 'get_file' e seguir as seguintes etapas: 
+                - Criar o script DDL em SQLite3 para criação da tabela seguindo os critérios listados abaixo e executar a criação utilizando a tool 'tool_executar_comando_sql'.
+                - Por cada linha apresentada na planilna, gerar o script de INSERT e executá-lo utilizando a tool 'tool_executar_comando_sql'.
+                - Ao executar todas as planilhas, retorne uma List[Str] contendo o script DDL de criação de todas as tabelas criadas.
+               ### CRITÉRIOS PARA NOMENCLATURA DAS TABELAS E COLUNAS:
+                - O nome da tabela deverá ser o nome do arquivo seguindo os demais critérios aqui estipulados.
+                - O nome de cada coluna deverá ser o nome apresentado na primeira linha seguindo os demais critérios aqui estipulados.
+                - O nome da tabela e colunas não deverá conter caracteres especiais, para isso, substitua-os pelo caracter referente. Exemplo: 'Ç' => 'c'; 'â' => 'a'; 'Õ' => 'o'; 'É' => 'e'.
+                - Removas os espaços exedentes (inclusive no início ou fim do nome).
+                - Utilizar apenas caracteres minúsculos (lower).  
+                - Substitua os espaços entre as palavras por '_'.
+                - Para o arquivo 'Base dias uteis.xlsx', ignorar a primeira linha, obtendo o nome das colunas conforme o apresentado na segunda linha.
+                - Ignorar o arquivo 'VR MENSAL 05.2025.xlsx'. 
+               ### PONTOS DE ATENÇÃO
+                - Ao acionar a função 'tool_executar_comando_sql', envie apenas a query SQL a ser executada, sem nenhum texto ou caractere adicional.
+                - Poderá utilizar a tool 'tool_executar_consulta' para efetuar consultas e avaliar seus conteúdos.
+            """
+                       )
+    )
+
+    @agente.tool
+    def get_file(ctx: RunContext[str], filename:str) -> str:
+        for fileblack in BLACK_LIST:
+            if str(filename).endswith(fileblack):
+                return
+        hd2 = False
+        for filehd2 in FILE_LIST_HEADER2:
+            if str(filename).endswith(filehd2):
+                hd2 = True
+        if hd2:
+            df = pd.read_excel(filename, skiprows=1, header=0)
+        else:
+            df = pd.read_excel(filename)
+
+        return df.to_csv()
+
+    @agente.tool
+    def tool_executar_comando_sql(ctx: RunContext[str], query: str):
+        try:
+            print(f'tool_executar_comando_sql - SQL gerado: \n{query}\n\n')
+            conn = sqlite3.connect(BANCO_DADOS)
+            cursor = conn.execute(query)
+            conn.commit()
+            # resultados = cursor.fetchall()
+            # colunas = [desc[0] for desc in cursor.description]
+            # return [dict(zip(colunas, linha)) for linha in resultados]
+        except Exception as ex:
+            print(f'@@@ Error ao executar consulta.\n'
+                  f'   @@@ {type(ex)}\n'
+                  f'   @@@{ex=}')
+        finally:
+            conn.close()
+
+    @agente.tool
+    def tool_executar_consulta(ctx: RunContext[str], query: str) -> List[Dict]:
+        try:
+            print(f'tool_executar_consulta - SQL gerado: \n{query}\n\n')
+            conn = sqlite3.connect(BANCO_DADOS)
+            cursor = conn.execute(query)
+            resultados = cursor.fetchall()
+            colunas = [desc[0] for desc in cursor.description]
+            return [dict(zip(colunas, linha)) for linha in resultados]
+        except Exception as ex:
+            print(f'@@@ Error ao executar consulta.\n'
+                  f'   @@@ {type(ex)}\n'
+                  f'   @@@{ex=}')
+        finally:
+            conn.close()
+
+    @agente.instructions
+    async def get_file_list(ctx: RunContext[XLSFileList]) -> str:
+        return str(ctx.deps.xlslist)
+
+    deps = XLSFileList(xlslist=xlslist)
+    retorno_load = agente.run_sync('Processar esta lista de arquivos.', deps=deps)
+    print(retorno_load.output)
+
 def agente_consulta_query() -> Agent:
     model = GoogleModel(
-        'gemini-2.5-flash', provider=GoogleProvider(api_key=os.environ.get('gem_key'))
+        'gemini-2.0-flash', provider=GoogleProvider(api_key=os.environ.get('gem_key'))
     )
 
     agente = Agent(
@@ -198,4 +338,7 @@ def main():
 
 if __name__ == "__main__":
     load_dotenv()
-    main()
+    # main()
+    retorno_extracao = agente_extracao(ARQUIVO_ZIP)
+
+    retorno_load = agente_load(retorno_extracao.filelist)
